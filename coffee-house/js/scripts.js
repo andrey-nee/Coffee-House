7c9a5434fb10da7dc783b65c32dba586aca5c507
andrey-nee <andrey.nee91@gmail.com>
2023-12-11 20:23:14 +0500

    fix: bug-fix burger-menu link click

--- coffee-house/js/scripts.js
+++ coffee-house/js/scripts.js
@@ -2,12 +2,35 @@ const page = document.getElementById('page-body');
 const burger = document.getElementById('burger');
 const navMain = document.getElementById('main-nav');
 const navMenu = document.getElementById('menu-link');
+let navLinks = document.getElementsByClassName('text-burger-link');
 
-burger.addEventListener("click", burgerMenu);
+// Открыть/Закрыть меню-бургер
+burger.addEventListener("click", function() {
+  // Закрыть если открыто
+  if (burger.classList.contains('burger__open')) {
+    burgerMenuClose();
+  }
+  // Открыть
+  else {
+    burgerMenuOpen();
+  }
+});
 
-function burgerMenu () {
-  burger.classList.toggle('burger__open');
-  navMain.classList.toggle('active');
-  navMenu.classList.toggle('active');
-  page.classList.toggle('lock');
+// Закрыть меню-бургер при переходе по ссылкам
+for (let navLink of navLinks) {
+  navLink.addEventListener("click", burgerMenuClose);
+}
+
+function burgerMenuOpen () {
+  burger.classList.add('burger__open');
+  navMain.classList.add('active');
+  navMenu.classList.add('active');
+  page.classList.add('lock');
+}
+
+function burgerMenuClose () {
+  burger.classList.remove('burger__open');
+  navMain.classList.remove('active');
+  navMenu.classList.remove('active');
+  page.classList.remove('lock');
 }

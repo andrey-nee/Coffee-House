b26604e684478f92bee2d0cf8b55c16a71ada823
andrey-nee <andrey.nee91@gmail.com>
2023-12-11 18:33:45 +0500

    feat: add burger script

--- coffee-house/js/scripts.js
+++ coffee-house/js/scripts.js
@@ -1,1 +1,13 @@
-// Scripts
+const page = document.getElementById('page-body');
+const burger = document.getElementById('burger');
+const navMain = document.getElementById('main-nav');
+const navMenu = document.getElementById('menu-link');
+
+burger.addEventListener("click", burgerMenu);
+
+function burgerMenu () {
+  burger.classList.toggle('burger__open');
+  navMain.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  page.classList.toggle('lock');
+}
